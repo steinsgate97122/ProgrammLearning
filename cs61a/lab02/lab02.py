65d65e70d112d1b3affb920d6648f0917ee7fd22
haohao_1522 <56289825+steinsgate97122@users.noreply.github.com>
2024-05-11 14:43:25 +0800

    Update lab02.py

--- cs61a/lab02/lab02.py
+++ cs61a/lab02/lab02.py
@@ -133,3 +133,80 @@ def cycle(f1, f2, f3):
             return res
         return h
     return g
+
+
+def make_keeper(n):
+    """Returns a function that takes one parameter cond and prints
+    out all integers 1..i..n where calling cond(i) returns True.
+
+    >>> def is_even(x): # Even numbers have remainder 0 when divided by 2.
+    ...     return x % 2 == 0
+    >>> make_keeper(5)(is_even)
+    2
+    4
+    >>> make_keeper(5)(lambda x: True)
+    1
+    2
+    3
+    4
+    5
+    >>> make_keeper(5)(lambda x: False)  # Nothing is printed
+    """
+    "*** YOUR CODE HERE ***"
+    def cond(f):
+        i = 1
+        while i <= n:
+            if f(i):
+                print(i)
+            i += 1
+    return cond
+
+
+def find_digit(k):
+    """Returns a function that returns the kth digit of x.
+
+    >>> find_digit(2)(3456)
+    5
+    >>> find_digit(2)(5678)
+    7
+    >>> find_digit(1)(10)
+    0
+    >>> find_digit(4)(789)
+    0
+    """
+    assert k > 0
+    "*** YOUR CODE HERE ***"
+    def find_kth_digit(x):
+        i, digit = 1, 0
+        while i <= k:
+            x, digit = x // 10, x % 10
+            i += 1
+        return digit
+    return find_kth_digit
+
+
+def match_k(k):
+    """Returns a function that checks if digits k apart match.
+
+    >>> match_k(2)(1010)
+    True
+    >>> match_k(2)(2010)
+    False
+    >>> match_k(1)(1010)
+    False
+    >>> match_k(1)(1)
+    True
+    >>> match_k(1)(2111111111111111)
+    False
+    >>> match_k(3)(123123)
+    True
+    >>> match_k(2)(123123)
+    False
+    """
+    def check(x):
+        while x // (10 ** k) > 0:
+            if (x // 10 ** k) % 10 != x % 10:
+                return False
+            x //= 10
+        return True
+    return check
